f776d6193accbca0fc9520d79872517659d0bea4
Ethan <ethansagin@gmail.com>
2019-03-19 16:21:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -21,10 +21,12 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet",
+        age: "15"
         },
       :heroine_friends => []
    }
