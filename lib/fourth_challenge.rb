1db4d1f77d4c64e8890c4ab48824c383311d5b11
Ethan <ethansagin@gmail.com>
2019-03-19 16:37:33 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,10 +5,8 @@
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"
-        },
-        {name: "Mercutio", age: "18", attitude: "hot-headed"
-        }
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -17,9 +15,7 @@
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44",
-          attitude: "worried"
-        }
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
